26178f1a602de94a1110fc8d2990a21f961c967a
KEREM-GAMERZ <a_sarioglu@outlook.com>
2024-07-28 12:22:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+genc_sozluk = {
+    "itidal":"soğukkanlı",
+    "fiktif": "suni, yapay",
+    "string": "karakterler listesi",
+    "encoding": "bir karakterin bitler (0 ve 1) ile temsili"
+}
+
+yeni_terim = input("Eklemek istediğin terim nedir\n")
+yeni_anlam = input("Kelimenin anlamı nedir?\n")
+
+genc_sozluk[yeni_terim] = yeni_anlam
+
+sorgu = input("Hangi kelimenin anlamını öğrenmek istersin?")
+print(genc_sozluk[sorgu])
+input()
